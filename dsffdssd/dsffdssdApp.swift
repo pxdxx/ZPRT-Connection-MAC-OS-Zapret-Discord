0307cae8c6ddff3a1ef959b4cf83233947f6e4ae
pxdx <140118197+pxdxx@users.noreply.github.com>
2026-09-11 22:37:51 +0300

    Initial Commit

---
+++ dsffdssd/dsffdssdApp.swift
@@ -0,0 +1,17 @@
+//
+//  dsffdssdApp.swift
+//  dsffdssd
+//
+//  Created by Tractor on 11.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct dsffdssdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
